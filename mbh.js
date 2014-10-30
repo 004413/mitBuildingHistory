d5944effddf44435cd3d43138a21304a54592cfe
Ray Hua Wu <dzaefn@mit.edu>
2014-10-29 23:26:22 -0400

    began work on writing in data

---
+++ mbh.js
@@ -0,0 +1,15 @@
+/* returns the zero-indexed year of MIT given a gregorian year */
+function cgtm(gregorianYear){
+  return gregorianYear - 1861;
+}
+
+/* startYear year of institvte started
+ * endYear year of institvte ended
+ * size physical area building takes up
+ * horizOffset offset on the graph */
+function drawBuilding(startYear,endYear,size,horizOffset){
+}
+
+/* cgtm(2014) used for 'present' */
+/* FILL OUT OTHER VARIABLES LATER */
+var b1 = drawBuilding(cgtm(1917),cgtm(2014))
